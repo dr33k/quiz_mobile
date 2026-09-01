47f9e9418cc825c70f3744753dc293f2b036a45f
dr33k <sevenpointzerozero@gmail.com>
2026-09-01 16:08:47 +0100

    Initial Commit : Designed and implemented MainActivity's view and logic

---
+++ app/src/main/java/com/example/quiz_mobile/activities/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.quiz_mobile.activities
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.quiz_mobile.R
+import com.example.quiz_mobile.util.Constants
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var namePromptTextView: TextView
+    private lateinit var nameEditText: EditText
+    private lateinit var startButton: Button
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        namePromptTextView = findViewById(R.id.namePromptTextView)
+        nameEditText = findViewById(R.id.nameEditText)
+        startButton = findViewById(R.id.startButton)
+
+        startButton.setOnClickListener {
+            namePromptTextView.text = getString(R.string.name_prompt_text)
+            namePromptTextView.setTextColor(Color.BLACK)
+
+            val playerName = nameEditText.text.toString()
+
+            if(playerName.isBlank()){
+                namePromptTextView.text = getString(R.string.empty_input_error_text)
+                namePromptTextView.setTextColor(Color.RED)
+                return@setOnClickListener
+            }
+
+            Intent(this@MainActivity, QuizActivity::class.java).apply{
+                with(this){
+                    putExtra(Constants.PLAYER_NAME_KEY, playerName)
+                }
+            }.also { startActivity(it) }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/quiz_mobile/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.quiz_mobile.util
+
+class Constants{
+    companion object{
+        const val PLAYER_NAME_KEY = "PLAYER_NAME"
+    }
+}
