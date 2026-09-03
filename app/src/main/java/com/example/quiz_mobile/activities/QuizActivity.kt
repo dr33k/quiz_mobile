b8598ed7fd369510cac46f16b8b3d1b6e795820a
dr33k <sevenpointzerozero@gmail.com>
2026-09-03 12:13:11 +0100

    Designed xml layout for QuizActivity and binded them to activity backend

--- app/src/main/java/com/example/quiz_mobile/activities/QuizActivity.kt
+++ app/src/main/java/com/example/quiz_mobile/activities/QuizActivity.kt
@@ -1,13 +1,28 @@
 package com.example.quiz_mobile.activities
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.quiz_mobile.R
+import com.example.quiz_mobile.util.Constants
 
 class QuizActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var imageView: ImageView
+    private lateinit var optionButton1: Button
+    private lateinit var optionButton2: Button
+    private lateinit var optionButton3: Button
+    private lateinit var optionButton4: Button
+    private lateinit var checkButton: Button
+
+    private val questionCount = 20
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +32,17 @@ class QuizActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        progressBar = findViewById(R.id.progressBar)
+        imageView = findViewById(R.id.imageView)
+        optionButton1 = findViewById(R.id.optionButton1)
+        optionButton2 = findViewById(R.id.optionButton2)
+        optionButton3 = findViewById(R.id.optionButton3)
+        optionButton4 = findViewById(R.id.optionButton4)
+        checkButton = findViewById(R.id.quizCheckButton)
+
+
+        val countryCodeList = Constants.COUNTRY_MAP.keys.toList()
+
     }
 }
