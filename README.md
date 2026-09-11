c1d64bc3426ab835ac3cbf96d770c661f538c789
dr33k <sevenpointzerozero@gmail.com>
2026-09-11 17:51:07 +0100

    Added retry button in ResultsActivity to navigate to MainActivity after quiz is over

--- README.md
+++ README.md
@@ -22,3 +22,5 @@ Kotlin: 2.4.20
 ![Screenshot (17).png](./md_images/Screenshot17.png)
 
 ![Screenshot (18).png](./md_images/Screenshot18.png)
+
+![Screenshot19.png](md_images/Screenshot19.png)
--- app/src/main/java/com/seven/learn/activities/ResultsActivity.kt
+++ app/src/main/java/com/seven/learn/activities/ResultsActivity.kt
@@ -1,6 +1,8 @@
 package com.seven.learn.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +14,7 @@ import com.seven.learn.util.Utils
 class ResultsActivity : AppCompatActivity() {
     private lateinit var resultsTextView: TextView
     private lateinit var scoreTextView: TextView
+    private lateinit var  retryButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +28,18 @@ class ResultsActivity : AppCompatActivity() {
 
         resultsTextView = findViewById(R.id.resultsTextView)
         scoreTextView = findViewById(R.id.scoreTextView)
+        retryButton = findViewById(R.id.retryButton)
 
         val player = Utils.extractPlayerExtra(intent)
 
         resultsTextView.text = getString(R.string.results_text, player?.name ?: "")
         scoreTextView.text = "${player?.score ?: 0}"
+        retryButton.setOnClickListener {
+            Intent(this@ResultsActivity, MainActivity::class.java)
+                .let {
+                    startActivity(it)
+                    finish()
+                }
+        }
     }
 }
